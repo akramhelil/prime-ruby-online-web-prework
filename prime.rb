2de7c10a4353c25f464d20a0cf93f2591b4f4a16
Akram Helil <akramhelil@gmail.com>
2019-01-11 21:23:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
   if number < 0 or number ==0 or number ==1
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor| ##did not understand this part.
-      num % possible_factor != 0
+    (2..number-1).to_a.all? do |possible_factor| ##did not understand this part.
+      number % possible_factor != 0
     end
   end
 end
