f973a88041f0aa8e9c34e5cdb9821b7796314380
Akram Helil <akramhelil@gmail.com>
2019-01-11 21:21:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,9 @@
 def prime?(number)
   if number < 0 or number ==0 or number ==1
     return false
-  else  
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end  
 end
